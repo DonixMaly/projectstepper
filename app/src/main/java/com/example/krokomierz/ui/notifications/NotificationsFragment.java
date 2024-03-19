d2dd8e5830141b03b122d05f2fd6d0cf41178479
Michał Iskra <michaliskra3008@gmail.com>
2024-03-19 18:22:22 +0100

    initial commit

---
+++ app/src/main/java/com/example/krokomierz/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,34 @@
+package com.example.krokomierz.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.krokomierz.databinding.FragmentNotificationsBinding;
+
+public class NotificationsFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        NotificationsViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
